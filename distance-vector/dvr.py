6572cfd4e92f83b862c14ee380b587554c64c04a
Chloe2330 <chloe.lin1577@gmail.com>
2024-12-29 06:08:45 -0800

    feat: Finished DVR v1

--- distance-vector/dvr.py
+++ distance-vector/dvr.py
@@ -4,15 +4,17 @@ from main import main
 async def run_dvr():
     routers = main()
 
+    print("Initial routing state")
     for router in routers.values():
         print(f"{router.id}: {router.vector}")
 
     tasks = []
     for router in routers.values():
-        tasks.append(router.dvr(routers))
+        tasks.append(router.run_dvr(routers))
 
     await asyncio.gather(*tasks)
 
+    print("Final routing state")
     for router in routers.values():
         print(f"{router.id}: {router.vector}")
     
--- network/router.py
+++ network/router.py
@@ -5,13 +5,9 @@ class Router:
     def __init__(self, id, num_routers, neighbors):
         self.id = id
         self.vector = [(inf, "") for _ in range(num_routers)]
-        self.set_self_index()
+        self.vector[int(self.id)] = (0, self.id)
         self.neighbors = neighbors
         self.queue = asyncio.Queue()
-    
-    def set_self_index(self):
-        index = int(self.id)
-        self.vector[index] = (0, self.id)
 
     def initialize_distance_vector(self):
         for neighbor_id, cost in self.neighbors.items():
@@ -22,23 +18,29 @@ class Router:
         for neighbor_id in self.neighbors:
             await routers[neighbor_id].queue.put((self.id, self.vector))
     
-    async def receive_distance_vector(self):
-        sender_id, distance_vector = await self.queue.get()
-        await self.update_distance_vector(sender_id, distance_vector)
+    async def receive_distance_vector(self, routers):
+        while True: 
+            try:
+                sender_id, distance_vector = await asyncio.wait_for(self.queue.get(), timeout=2)
+                cost = self.neighbors[sender_id]
+
+                # Bellman-Ford
+                for i in range(len(self.vector)):
+                    new_cost = cost + distance_vector[i][0]
+                    if new_cost < self.vector[i][0]:
+                        self.vector[i] = (new_cost, sender_id)
+
+                        print(f"Router {self.id} vector is updated using Router {sender_id} vector.")
+                        for router in routers.values():
+                            print(f"{router.id}: {router.vector}")
+                        await self.send_distance_vector(routers)
     
-    async def update_distance_vector(self, sender_id, distance_vector):
-        updated = False
-        cost = self.neighbors[sender_id]
+            except asyncio.TimeoutError:
+                break
 
-        # Bellman-Ford
-        for i in range(len(self.vector)):
-            new_cost = cost + distance_vector[i][0]
-            if new_cost < self.vector[i][0]:
-                self.vector[i] = (new_cost, sender_id)
-                updated = True 
-
-        return updated 
-
-    async def dvr(self, routers):
-        await asyncio.gather(self.send_distance_vector(routers), self.receive_distance_vector())
-        
+    async def run_dvr(self, routers):
+        await asyncio.gather(
+            self.send_distance_vector(routers),
+            self.receive_distance_vector(routers)
+        )
+    
