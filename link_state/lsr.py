4b303551053a20948f38c598f3f01bff966eb147
Chloe2330 <chloe.lin1577@gmail.com>
2025-01-08 03:44:03 -0800

    feat: Dijkstra's for LSR

---
+++ link_state/lsr.py
@@ -0,0 +1,49 @@
+import asyncio 
+from main import main
+from main import update_network_continuously
+from utils.signal_handler import setup_signal_handler, get_stop_event
+
+async def run_lsr(network, routers):
+
+    print("Initial routing state")
+    for router in routers.values():
+        print(f"{router.id}: {router.vector}")
+
+    tasks = []
+    for router in routers.values():
+        tasks.append(router.run_lsr(network))
+
+    await asyncio.gather(*tasks)
+
+    print("Routing state after initial convergence")
+    for router in routers.values():
+        print(f"{router.id}: {router.vector}")
+
+    print("Listening for changes in the config file...")
+
+    # post_poison_reverse = False 
+
+    # stop_event = get_stop_event()
+    # while not stop_event.is_set(): 
+    #     await asyncio.sleep(1)
+    #     updated, new_network = await asyncio.create_task(update_network_continuously(network, routers))
+    #     network = new_network
+    #     if updated or post_poison_reverse:
+    #         post_poison_reverse = False 
+    #         print("Change detected!")
+    #         for router in routers.values():
+    #             print(f"{router.id}: {router.vector}")
+            
+    #         tasks = []
+    #         for router in routers.values():
+    #             tasks.append(router.run_dvr(routers))
+    #         await asyncio.gather(*tasks)
+
+async def lsr_main():
+    setup_signal_handler()
+    network, routers = main()
+    await run_lsr(network, routers)
+    
+if __name__ == "__main__":
+    asyncio.run(lsr_main())
+    
--- network/router.py
+++ network/router.py
@@ -1,4 +1,5 @@
 import asyncio
+import heapq
 from math import inf
 from distance_vector.poison_reverse import state_manager
 
@@ -57,4 +58,28 @@ class Router:
             self.send_distance_vector(routers),
             self.receive_distance_vector(routers)
         )
+
+    async def run_dijkstra(self, network):
+        start = self.id
+        visited = set()
+        pq = []
+
+        heapq.heappush(pq, (self.vector[start][0], start)) # push (distance, router ID) 
+
+        while pq:
+            current_distance, current_router = heapq.heappop(pq)
+
+            if current_router in visited:
+                continue
+
+            visited.add(current_router)
+
+            for neighbor, weight in network.links[current_router].items():
+                new_distance = current_distance + weight
+                if new_distance <= self.vector[neighbor][0]:
+                    self.vector[neighbor] = (new_distance, current_router) 
+                    heapq.heappush(pq, (new_distance, neighbor))
+
+    async def run_lsr(self, network):
+        await asyncio.gather(self.run_dijkstra(network))
     
